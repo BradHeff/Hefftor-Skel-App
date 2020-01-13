54e47e65651030b801c7bb7fe54bd373c26db506
Brad Heffernan <ph3onix83@gmail.com>
2020-01-13 11:27:48 +1030

    update

--- .local/share/Hefftor Skel App/Hefftor-Skel-App.py
+++ .local/share/Hefftor Skel App/Hefftor-Skel-App.py
@@ -160,7 +160,7 @@ class HSApp(Gtk.Window):
 
 
 def copytree(src, dst, symlinks=False, ignore=None):
-    if os.path.exists(dst):
+    if not os.path.exists(dst):
         os.makedirs(dst)
     for item in os.listdir(src):
         s = os.path.join(src, item)
